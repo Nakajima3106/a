841e03094a01ce9bda743630fc837b4a79229c91
Nakajima3106 <ec2-user@ip-172-31-37-89.ec2.internal>
2020-11-09 07:30:56 +0000

    secondcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :books
+  
+  root to: 'books#index'
+  
+  post 'books' => 'books#create'
 end
